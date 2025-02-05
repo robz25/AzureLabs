26c7def601df41f530f5c9a2ae854449caa53eae
Robin <weiserfi@outlook.com>
2025-02-05 12:21:36 -0600

    Working to fetch logs from App Insights, local settings may not work

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Channel;
+
+namespace SimpleWebJob
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureWebJobs(b =>
+                {
+                    b.AddAzureStorageBlobs();
+                    b.AddAzureStorageQueues();
+                })
+                .ConfigureLogging((context, b) =>
+                {
+                    b.AddConsole();
+                    //b.AddApplicationInsightsWebJobs(o => o.ConnectionString = Environment.GetEnvironmentVariable("InstrumentationKey=fa8c28b8-ebfd-42e5-88e3-c48f1fdcebad;IngestionEndpoint=https://canadacentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://canadacentral.livediagnostics.monitor.azure.com/;ApplicationId=405de63e-1df7-4482-a6c9-03829bfa0628"));
+                    b.AddApplicationInsightsWebJobs(o => o.ConnectionString = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING"));
+                    b.AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider>("", LogLevel.Information);
+                })
+                .ConfigureServices(s =>
+                {
+                    s.AddSingleton<ITelemetryInitializer, MyTelemetryInitializer>();
+                })
+                .Build();
+
+            using (host)
+            {                
+               var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+                logger.LogInformation("WebJob started");
+
+                // Simulate some work
+                await Task.Delay(5000);
+
+                logger.LogInformation("WebJob completed successfully");
+
+                // Stop the host after the job completes
+                await host.StopAsync();
+            }
+        }
+    }
+
+    public class MyTelemetryInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Cloud.RoleName = "MyWebJob6";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# AzureLabs
-Compilation of demos, or experiments to achieve specific objectives on Azure public cloud
+# .net9 WebJob to run on Azure App Service
+
+## Creating the webjob
+
+```bat
+dotnet new console # creates new console app
+dotnet build # buisl project
+dotnet run # runs project
+
+#Run this to generate executable with all dlls in publish folder
+dotnet publish -c Release -o ./
+publish                                    
+
+#Then create a run.cmd batch file, contents:
+@echo off
+ExecFileName.exe
+
+# put that file inside the publish folder and compress to zip
+# then upload to Azure in webJobs view
+```
+
+## Local variables for testing
+
+Create local.settings.json in project root
+Add any env vars inside
+```
+{
+  "IsEncrypted": false,
+  "Values": {
+    "APPINSIGHTS_CONNECTIONSTRING": "your-connection-string"
+  }
+}
+```
+
+Refer to them on code
+
+```c#
+string appInsightsConnectionString = Environment.GetEnvironmentVariable("APPINSIGHTS_CONNECTIONSTRING");
+```
+
+## Query logs in application insights
+
+```kql
+traces
+| where cloud_RoleName == "MyWebJob6"
+| project timestamp, message
+```
+
+## Quick test
+
+In this case the ai6wj.zip is already included we just need to upload this file to azure and make sure our app has the setting APPLICATIONINSIGHTS_CONNECTION_STRING
